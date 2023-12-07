f9246a12ba18a339423d46830a07ed6c0504650b
João Gabriel <joaogabriel9633@gmail.com>
2023-12-07 08:12:18 -0300

    Salved

--- FastAPI/Code/2-pydantic.py
+++ FastAPI/Code/2-pydantic.py
@@ -1,1 +1,22 @@
-user = {"nome": "Fulano", "idade": 25, "email": "fulano@gmail.com"}
+# user = {"nome": "Fulano", "idade": 25, "email": "fulano@gmail.com"}
+
+# print(user)
+
+from pydantic import BaseModel, field_validator
+
+
+class User(BaseModel):
+    name: str
+    age: int
+    email: str
+
+    @field_validator("email")
+    def validate_email(cls, value):
+        if "@" not in value:
+            raise ValueError("E-mail está inválido")
+        return value
+
+
+user1 = User(name="Fulano", age=23, email="fulano@gmail.com")
+
+print(user1)
