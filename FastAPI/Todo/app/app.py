e4c596d75f4c71d9e4bcc5dd31a959fc54dfdf06
JGabriel963 <joaogabriel9633@gmail.com>
2024-01-05 07:11:33 -0300

    Initialize project with FastApi

--- FastAPI/Todo/app/app.py
+++ FastAPI/Todo/app/app.py
@@ -1,24 +1,7 @@
 from fastapi import FastAPI
-from core.config import settings
-from beanie import init_beanie
-from motor.motor_asyncio import AsyncIOMotorClient
-from models.user_model import User
 
-app = FastAPI(
-    title=settings.PROJECT_NAME,
-    openapi_url=f'{settings.API_V1_STR}/openapi.json'
-)
+app = FastAPI()
 
-@app.on_event('startup')
-async def app_startup():
-    client_db = AsyncIOMotorClient(
-        settings.MONGO_CONNECTION
-    )
-
-    await init_beanie(
-        database=client_db,
-        document_models=[
-            User,
-        ]
-    )
-
+@app.get("/")
+async def hello():
+    return 'Hello World'
