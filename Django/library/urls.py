cb5a7ea0fd5410958cc4b77986c6e6df3e8721f8
JGabriel963 <joaogabriel9633@gmail.com>
2024-03-03 22:58:28 -0300

    Apresentando mensagens no Django

--- Django/library/urls.py
+++ Django/library/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.http import HttpResponse
-from library.views import home
+from library.views import home, logout_user
 
 urlpatterns = [
-    path("", home, name='home')
+    path("", home, name='home'),
+    path("logout/", logout_user, name='logout')
 ]
--- Django/library/views.py
+++ Django/library/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
-    # return HttpResponse('Teste Home')
-    return render(request, "home.html")
+    if request.method == 'POST':
+        username = request.POST['usuario']
+        password = request.POST['senha']
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login foi realizado com sucesso!")
+
+            return redirect('home')
+        else:
+            messages.error(
+                request,
+                "Erro na autenticação. Tente novamente!"
+            )
+
+            return redirect('home')
+
+
+    else:
+        return render(request, 'home.html')
+
+
+def logout_user(request):
+    pass
