9c0912e2f9d25bd3f195feff0954360bdd17d24a
JGabriel963 <joaogabriel9633@gmail.com>
2024-04-18 07:56:35 -0300

    Get current user

--- Todo/app/api/auth/jwt.py
+++ Todo/app/api/auth/jwt.py
@@ -3,10 +3,14 @@ from fastapi.security import OAuth2PasswordRequestForm
 from typing import Any
 from services.user_service import UserService
 from core.security import create_acess_token, create_refresh_token
+from schemas.auth_schema import TokenSchema
+from schemas.user_schema import UserDetail
+from models.user_model import User
+from api.dependencies.user_deps import get_current_user
 
 auth_router = APIRouter()
 
-@auth_router.post('/login')
+@auth_router.post('/login', summary="Criar Acess Token e Refresh Token", response_model=TokenSchema)
 async def login(data: OAuth2PasswordRequestForm = Depends()) -> Any:
     user = await UserService.authenticate(
         email = data.username,
@@ -23,3 +27,7 @@ async def login(data: OAuth2PasswordRequestForm = Depends()) -> Any:
         "acess_token": create_acess_token(user.user_id),
         "refresh_token": create_refresh_token(user.user_id)
     }
+
+@auth_router.post('/test_token', summary="Testando o Token", response_model=UserDetail)
+async def test_token(user: User = Depends(get_current_user)):
+    return user
--- Todo/app/services/user_service.py
+++ Todo/app/services/user_service.py
@@ -3,6 +3,7 @@ from schemas.user_schema import UserAuth
 from core.security import get_password_hash
 from core.security import verify_password
 from typing import Optional
+from uuid import UUID
 
 class UserService:
     @staticmethod
@@ -19,6 +20,11 @@ class UserService:
     @staticmethod
     async def get_user_by_email(email: str) -> Optional[User]:
         user = await User.find_one(User.email == email)
+        return user
+    
+    @staticmethod
+    async def get_user_by_id(id: UUID) -> Optional[User]:
+        user = await User.find_one(User.user_id == id)
         return user
 
     @staticmethod
