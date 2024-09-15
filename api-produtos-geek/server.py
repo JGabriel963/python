f2c8bd0a7930d5d978dff4664db36ef4bc827b04
João Gabriel <joaogabriel9633@gmail.com>
2024-09-15 19:56:39 -0300

    finish project

---
+++ api-produtos-geek/server.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from database.db import sync_database, get_engine
+from models.product import ProductCreate, Product
+from controllers.products_controllers import router
+
+app = FastAPI()
+
+async def get_db():
+    yield sync_database(get_engine())
+
+app.include_router(router, prefix='/products')
+
+  
